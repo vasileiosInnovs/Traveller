ad462a6f860ff81abecdb374fd8d0cb39c7fc752
Hagee <hagee.ktr@outlook.com>
2025-12-01 18:13:15 +0300

    build app boilerplate code

---
+++ backend/app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_migrate import Migrate
+from app.extensions.database import db
+
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
